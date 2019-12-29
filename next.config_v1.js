2b5b4559bba59932aefa0cc21460a9d3c8261463
scottie-schneider <scott@laclave.co>
2019-12-29 16:30:41 -0500

    using diff next config

---
+++ next.config_v1.js
@@ -0,0 +1,27 @@
+// next.config.js
+const withOffline = require('next-offline')
+
+const nextConfig = {
+  workboxOpts: {
+    runtimeCaching: [
+      {
+        urlPattern: /.png$/,
+        handler: 'CacheFirst'
+      },
+      {
+        urlPattern: /api/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheableResponse: {
+            statuses: [0, 200],
+            headers: {
+              'x-test': 'true'
+            }
+          }
+        }
+      }
+    ]
+  }
+}
+
+module.exports = withOffline(nextConfig)
